87b7363251a6d72c5db3edc7540aa08057a0af61
ikeya <kazukii315aria@gmail.com>
2024-03-26 15:09:18 +0900

    初回追加

---
+++ PowerShell/JsonToJson_sub.ps1
@@ -0,0 +1,34 @@
+﻿param ([string]$scriptDirectory)
+
+Write-Host -Prompt "始まり"
+
+# スクリプトが配置されたディレクトリを取得
+if ($scriptDirectory -eq "") {
+	$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+}
+
+# 関数読込
+. "$scriptDirectory\functions.ps1" -scriptDirectory $scriptDirectory
+
+$convertRolePath = "./convertRule.json"
+$convertRole = Get-Content -Path $convertRolePath -Raw | ConvertFrom-Json
+
+# Tsvファイルを読み込む
+$fileListPath = "./filelist.tsv" 
+$fileList = Get-Content -Path $fileListPath | ConvertFrom-Csv -Delimiter "`t" -Header ("ImportPath")
+foreach ($row in $fileList) {
+    $targetFilePath = $row.ImportPath
+    $target = Get-Content -Path $targetFilePath -Raw | ConvertFrom-Json
+    
+    # 変換開始
+    $convertedTarget = ConvertJsonToJson $target $convertRole
+    
+    # ファイル出力
+    $fileName = Split-Path -Path $targetFilePath -Leaf
+    $outputPath = "./results/" + $fileName
+    $convertedTarget | ConvertTo-Json -Compress | Set-Content -Path $outputPath -Encoding UTF8
+    Write-Host $fileName
+}
+
+Read-Host -Prompt "終わり"
+
---
+++ PowerShell/functions.ps1
@@ -0,0 +1,122 @@
+# json変換
+function ConvertJsonToJson($target, $convertRole){
+    $convertedTarget = [ref]$target
+    foreach ($convertRoleItem in $convertRole.Items) {
+        # Keyの情報を取得する
+        $keys = $convertRoleItem.Key -split "\."
+    
+        # 対象を加工する
+        ConvertJsonToJsonSub $convertedTarget $keys $convertRoleItem    
+    }
+    return $convertedTarget.Value
+}
+
+# json変換実行部
+function ConvertJsonToJsonSub([ref]$convertedTarget, $keys, $convertRoleItem){
+    $key = $keys[0]
+    $value = $convertedTarget.Value.$key
+    if ($keys.Count -eq 1) {
+        # 項目の変換実行
+        ConvertJsonToJsonSubItem $convertedTarget $key $value $convertRoleItem
+        return 
+    }
+
+    # 後続のキーに移行する 
+    $nextKeys = $keys[1..($keys.Count - 1)]
+    if ($value.GetType().IsArray) {
+        $i = -1
+        foreach ($valueItem in $value) {
+            $i += 1
+            $nextValue = [ref]$valueItem
+            ConvertJsonToJsonSub $nextValue $nextKeys $convertRoleItem
+            $value[$i] = $nextValue.Value
+        }
+    } else {
+        $nextValue = [ref]$value
+        ConvertJsonToJsonSub $nextValue $nextKeys $convertRoleItem
+        $value = $nextValue.Value
+    }
+    $convertedTarget.Value.$key = $value
+}
+
+# json変換（項目部分）
+function ConvertJsonToJsonSubItem([ref]$convertedTarget, $key, $value, $convertRoleItem){
+    # 文字変換
+    if ($null -ne $convertRoleItem.ConvertPairs){
+        if ($null -ne $convertRoleItem.ConvertPairs.$value){
+            $value = $convertRoleItem.ConvertPairs.$value
+        }
+    }
+    # 文字置換
+    if ($null -ne $convertRoleItem.ReplacePairs){
+        foreach ($property in $convertRoleItem.ReplacePairs.PSObject.Properties) {
+            $replaceKey = $property.Name
+            $replaceValue = $property.Value
+            $value = $value -replace $replaceKey, $replaceValue
+        }
+    }
+    # 空判定
+    if ($null -ne $convertRoleItem.NullValue -and ($value -eq "" -or $value -eq $null)){
+        $value = $convertRoleItem.NullValue
+    }
+    # 文字列形式
+    if ($null -ne $convertRoleItem.StringCase){
+        $value = ConvertToCase $value $convertRoleItem.StringCase
+    }
+    # 結果格納
+    $newKey = $key
+    if ($null -ne $convertRoleItem.NewKey){
+        $newKey = $convertRoleItem.NewKey
+    }
+    if ($null -eq $convertedTarget.Value.$newKey){
+        $convertedTarget.Value | Add-Member -MemberType NoteProperty -Name $newKey -Value $value
+    }
+    $convertedTarget.Value.$newKey = $value
+}
+
+function ConvertToCase([string]$text, [string]$case){
+    if ($case.ToLower() -eq "snakecase"){
+        return ConvertToSnakeCase $text
+    } elseif ($case.ToLower() -eq "pascalcase"){
+        return ConvertToPascalCase $text
+    } elseif ($case.ToLower() -eq "camelcase"){
+        return ConvertToCamelCase $text
+    } else {
+        return $text
+    }
+}
+
+
+# snake_case形式に変換する関数
+function ConvertToSnakeCase([string]$text) {
+    if ($text -eq ""){
+        return ""
+    }
+    if ($text -match "[_-]") {
+        return $text -replace "[_-]", "_"
+    }
+    $snakeCase = [System.Text.RegularExpressions.Regex]::Replace($text, "(?<!^)(?=[A-Z])", "_$&").ToLower()
+    return $snakeCase.ToLower()
+}
+
+# PascalCase形式に変換する関数
+function ConvertToPascalCase([string]$text) {
+    if ($text -eq ""){
+        return ""
+    }
+    if (-not $text -match "[_-]") {
+        return $text.ToLower().Substring(0, 1).ToUpper() + $text.Substring(1).ToLower() 
+    }
+    $words = $text -split "[_-]"
+    $pascalCase = $words | ForEach-Object { $_.Substring(0, 1).ToUpper() + $_.Substring(1).ToLower() }
+    return $pascalCase -join ""
+}
+
+# camelCase形式に変換する関数
+function ConvertToCamelCase([string]$text)  {
+    if ($text -eq ""){
+        return ""
+    }
+    $pascalCase = ConvertToPascalCase $text
+    return $pascalCase.Substring(0, 1).ToLower() + $pascalCase.Substring(1)
+}
